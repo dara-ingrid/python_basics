c7236f64193925e9719a4da246979b93d465c530
Dara Ferreira <dara-ingrid@hotmail.com>
2023-04-26 15:07:58 -0300

    First steps with Python

---
+++ cap01/desafio_sleep.py
@@ -0,0 +1,18 @@
+"""
+Importe o módulo "time"
+Imprima na tela "Olá, mundo!"
+Insira a função "time.sleep" e passe como argumento 2
+Imprima na tela "Agora sou uma pessoa programadora Python." e "Aguarde 4.5 segundos"
+Insira a função "time.sleep" e passe como argumento 4.5
+Imprima na tela "Última linha do programa."
+
+"""
+
+import time 
+
+print("Olá, mundo!")
+time.sleep(2)
+print("Agora sou uma programadora Python!")
+print("Aguarde 4.5 segundos!")
+time.sleep(4.5)
+print("Última linha de comando")
